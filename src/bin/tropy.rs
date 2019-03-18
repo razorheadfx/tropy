d0b68bce9d0e646e3b99bc2834aca606e9ff3801
girke <felix.girke@tu-dortmund.de>
2019-03-18 17:56:46 +0100

    Clarify CLI commands (--csv vs. --raw)

--- src/bin/tropy.rs
+++ src/bin/tropy.rs
@@ -1,77 +1,102 @@
-extern crate structopt;
 extern crate colorful;
+extern crate structopt;
 
-use colorful::{HSL,Colorful};
+use colorful::{Colorful, HSL};
 use structopt::StructOpt;
 
-use tropy::Calculator;
-use std::io;
-use std::io::{Write, Read,BufRead,BufReader};
 use std::fs::File;
+use std::io;
+use std::io::{BufRead, BufReader, Read, Write};
+use tropy::Calculator;
 
-
-#[derive(Debug,StructOpt)]
-struct Cfg{
-    #[structopt(long = "file", help = "File to be read for input. If no file is given, stdin is opened for reading")]
-    file : Option<String>,
-    #[structopt(short = "n", long = "chunksize", default_value = "1024", help = "The number of bytes to be used for each value")]
-    chunksize : u32,
-    #[structopt(long = "raw", help = "Output as csv to stdout in the format startpos;endpos;entropy")]
-    raw_values : bool
+#[derive(Debug, StructOpt)]
+struct Cfg {
+    #[structopt(
+        short = "f",
+        long = "file",
+        help = "File to be read for input. If no file is given, stdin is opened for reading"
+    )]
+    file: Option<String>,
+    #[structopt(
+        short = "n",
+        long = "chunksize",
+        default_value = "1024",
+        help = "The number of bytes to be read for each entropy calculation"
+    )]
+    chunksize: u32,
+    #[structopt(
+        short = "c",
+        long = "csv",
+        help = "Output as csv to stdout instead of using color-coding on the terminal. Format: <startbyte>;<endbyte>;<entropy>"
+    )]
+    csv: bool,
 }
 
-fn display(e : f64, col : usize){
-    if col % 80 == 0{
-        print!("\n");
+fn display(e: f64, col: usize) {
+    if col % 80 == 0 {
+        println!();
     }
     // scale entropy to bits (i.e. value/8)
     // i.e. perfectly uniform data would have an entropy of 1 (i.e. 8bits/byte)
-    let h = 240.0/360.0 + e / 8.0 * 120.0/360.0;
-    print!("{}","█".hsl(h as f32,1.0,0.5));
+    let h = 240.0 / 360.0 + e / 8.0 * 120.0 / 360.0;
+    print!("{}", "█".hsl(h as f32, 1.0, 0.5));
 }
 
-fn main() -> io::Result<()>{
+fn main() -> io::Result<()> {
     let cfg = Cfg::from_args();
-    let mut r : Box<dyn BufRead> = match cfg.file{
-        Some(f) =>{
-            let  s = File::open(&f)?;
-            eprintln!("* Using {} for input",f);
-            let  r = BufReader::with_capacity(2048usize,s);
+    let mut r: Box<dyn BufRead> = match cfg.file {
+        Some(f) => {
+            let s = File::open(&f)?;
+            eprintln!("* Using {} for input", f);
+            let r = BufReader::with_capacity(2048usize, s);
             Box::new(r)
-        },
-        None =>{
-            let  s = io::stdin();
+        }
+        None => {
+            let s = io::stdin();
             eprintln!("* Using stdin for input");
-            let  r = BufReader::with_capacity(2048usize,s);
+            let r = BufReader::with_capacity(2048usize, s);
             Box::new(r)
         }
     };
 
-
     let chunksize = cfg.chunksize as usize;
-    let mut buf = vec![0u8;chunksize];
+    let mut buf = vec![0u8; chunksize];
     let mut c = Calculator::new();
     let mut chunknum = 0usize;
-    
+
     eprintln!("* Using chunks of {}bytes", chunksize);
 
-    if !cfg.raw_values{
+    if !cfg.csv {
         // show colormap
-        eprintln!("* Color Scale: Entropy 0 {} 1", "█".repeat(80-27).gradient_with_color(HSL::new(240.0/360.0, 1.0,0.5), HSL::new(360.0/360.0, 1.0,0.5)));
+        eprintln!(
+            "* Color Scale: Entropy 0 {} 1",
+            "█"
+                .repeat(80 - 27)
+                .gradient_with_color(HSL::new(2.0 / 3.0, 1.0, 0.5), HSL::new(1.0, 1.0, 0.5))
+        );
+    } else {
+        // use raw data
+        eprintln!("Outputting raw data as csv in the format <startbyte>;<endbyte>;<entropy>");
+        println!("\"start\";\"end\";\"entropy\"");
     }
-    
-    while let Ok(_) = r.read_exact(&mut buf[..]).and_then(|_|c.write(&buf[..])){
+
+    while let Ok(_) = r.read_exact(&mut buf[..]).and_then(|_| c.write(&buf[..])) {
         // TODO: opt
         let e = c.entropy();
 
-        if !cfg.raw_values{
-            display(e,chunknum);
-        }else{
+        if !cfg.csv {
+            display(e, chunknum);
+        } else {
             // output as csv
-            println!("{};{};{:.3}",chunknum*chunksize, (chunknum+1)*chunksize,e);
+            println!(
+                "{};{};{:.6}",
+                chunknum * chunksize,
+                (chunknum + 1) * chunksize,
+                e
+            );
         }
-        
-        chunknum+=1;
+
+        chunknum += 1;
     }
 
     Ok(())
--- src/lib.rs
+++ src/lib.rs
@@ -1,56 +1,57 @@
-use std::io::Write;
 use std::io;
+use std::io::Write;
 
-pub struct Calculator{
+pub struct Calculator {
     // this is 16kB in size so it might blow the stack for certain configurations
     // hence boxed
-    counts : Box<[u64; 256]>,
-    nbytes : usize
+    counts: Box<[u64; 256]>,
+    nbytes: usize,
 }
 
-impl Calculator{
-    pub fn new() -> Self{
-        Calculator{
-            counts : Box::new([0u64;256]),
-            nbytes : 0
+impl Calculator {
+    pub fn new() -> Self {
+        Calculator {
+            counts: Box::new([0u64; 256]),
+            nbytes: 0,
         }
     }
 
     /// calculates Shannon entropy over the bytes written so far
     /// and clears the internal state
-    pub fn entropy(&mut self) -> f64{
+    pub fn entropy(&mut self) -> f64 {
         let bytes = self.nbytes as f64;
-        let e = self.counts.iter()
-            .filter(|c|*c > &0u64)
+        let e = self
+            .counts
+            .iter()
+            .filter(|c| *c > &0u64)
             .map(|c| *c as f64 / bytes)
             .map(|p| p * p.log2())
-            .fold(0.0, |h,x| h-x);
+            .fold(0.0, |h, x| h - x);
 
-        self.counts.iter_mut().for_each(|c|*c = 0u64);
+        self.counts.iter_mut().for_each(|c| *c = 0u64);
         self.nbytes = 0;
         e
     }
-
 }
 
-
-impl Write for Calculator{
-    fn write(&mut self, input : &[u8]) -> io::Result<usize>{
-        input.iter().for_each(|byte| 
-            self.counts[*byte as usize] = self.counts[*byte as usize].checked_add(1)
-                .expect("Count exceeded the length of a u64; Where'd you get that many bytes from?"));
+impl Write for Calculator {
+    fn write(&mut self, input: &[u8]) -> io::Result<usize> {
+        input.iter().for_each(|byte| {
+            self.counts[*byte as usize] = self.counts[*byte as usize]
+                .checked_add(1)
+                .expect("Count exceeded the length of a u64; Where'd you get that many bytes from?")
+        });
         let bytes = input.len();
         self.nbytes += bytes;
         Ok(bytes)
     }
 
-    fn flush(&mut self) -> io::Result<()>{
+    fn flush(&mut self) -> io::Result<()> {
         Ok(())
     }
 }
 
-
-pub fn slice_entropy(input : &[u8]) -> f64{
+pub fn slice_entropy(input: &[u8]) -> f64 {
     let mut c = Calculator::new();
 
     c.write(input).expect("This should never fail");
@@ -58,48 +59,45 @@ pub fn slice_entropy(input : &[u8]) -> f64{
     c.entropy()
 }
 
-pub fn hsl_to_rgb(h : f32, s : f32, l : f32) -> (u8,u8,u8){
-    assert!( 0.0 <= h && h <= 1.0);
-    assert!( 0.0 <= s && s <= 1.0);
-    assert!( 0.0 <= l && l <= 1.0);
+pub fn hsl_to_rgb(h: f32, s: f32, l: f32) -> (u8, u8, u8) {
+    // hsl to rgb conversion in the case i drop colorful in favour of termion
+    assert!(0.0 <= h && h <= 1.0);
+    assert!(0.0 <= s && s <= 1.0);
+    assert!(0.0 <= l && l <= 1.0);
 
     // calc chroma
-    if s == 0.0{
+    if s == 0.0 {
         let g = (255.0 * l) as u8;
-        (g,g,g)
-    }else{
-
-        let c = (1.0 - (2.0*l-1.0).abs())*s;
-        let x = c * (1.0 - (h / 6.0  % 2.0 - 1.0).abs());
-        let m = l - c/2.0;
+        (g, g, g)
+    } else {
+        let c = (1.0 - (2.0 * l - 1.0).abs()) * s;
+        let x = c * (1.0 - (h / 6.0 % 2.0 - 1.0).abs());
+        let m = l - c / 2.0;
         unimplemented!()
     }
-
 }
 
 #[cfg(test)]
-mod test{
+mod test {
     use crate::{hsl_to_rgb, slice_entropy};
     #[test]
-    fn hsl(){
-        assert_eq!(hsl_to_rgb(0.0, 1.0,0.5), (255,0,0));
-
+    fn hsl() {
+        assert_eq!(hsl_to_rgb(0.0, 1.0, 0.5), (255, 0, 0));
     }
-    
+
     #[test]
-    fn calculate_entropy(){
-        let x = [0u8,23u8,66u8,1u8];
+    fn calculate_entropy() {
+        let x = [0u8, 23u8, 66u8, 1u8];
         let e = slice_entropy(&x);
 
         assert_eq!(e, 2.0);
 
-        let x = [0u8,0u8,66u8,1u8];
+        let x = [0u8, 0u8, 66u8, 1u8];
         let e = slice_entropy(&x);
         assert_eq!(e, 1.5);
 
-        let x = [0u8,1u8];
+        let x = [0u8, 1u8];
         let e = slice_entropy(&x);
         assert_eq!(e, 1.0);
-
     }
 }
